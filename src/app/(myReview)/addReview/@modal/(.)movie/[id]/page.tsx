39d39fd001603686a50f4934d971109aaa2690e3
marie1016 <kjo5281@gmail.com>
2025-09-03 18:48:52 +0900

    fix: 리뷰등록 모달 디자인 수정

--- src/app/(myReview)/addReview/@modal/(.)movie/[id]/page.tsx
+++ src/app/(myReview)/addReview/@modal/(.)movie/[id]/page.tsx
@@ -16,9 +16,9 @@ export default async function ModalPage({
 
   return (
     <>
-      <div className="fixed left-0 top-0 z-20 h-full w-full bg-black/60" />
+      <div className="fixed inset-0 z-10 h-screen w-screen bg-black/60" />
       <dialog
-        className="z-30 mb-14 h-auto w-full rounded-xl p-7 sm:w-[480px] sm:p-14"
+        className="fixed inset-0 z-20 mt-20 h-auto w-full overflow-y-scroll rounded-xl p-7 sm:mb-14 sm:w-[480px] sm:p-14"
         open
       >
         <h2 className="text-center text-2xl font-medium">영화 리뷰 등록</h2>
