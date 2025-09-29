d9668add6c76fb3cb378c6db97bf5df1ba504a44
marie1016 <kjo5281@gmail.com>
2025-09-29 21:35:17 +0900

    fix: 빌드 에러 수정

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,20 +1,15 @@
-import {
-  cert,
-  getApps,
-  initializeApp,
-  ServiceAccount,
-} from "firebase-admin/app";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 const adminConfig = {
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(adminConfig as ServiceAccount),
+    credential: cert(adminConfig),
   });
 }
 
