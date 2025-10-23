d0e085ba35f9acd01cc86eb808c3f897a12595ec
marie1016 <kjo5281@gmail.com>
2025-10-23 23:26:28 +0900

    feat: 빈 화면 UI 추가

--- src/app/(myReview)/searchReviews/page.tsx
+++ src/app/(myReview)/searchReviews/page.tsx
@@ -16,15 +16,25 @@ export default function SearchReviewsPage() {
 
   return (
     <div className="mx-auto max-w-4xl">
-      <div className="grid grid-cols-1 justify-between gap-12 lg:grid-cols-[380px_380px] lg:gap-14">
-        {searchResults.map((review: Review) => (
-          <div key={review.id}>
-            <ReviewHeader {...review} />
-            <ReviewInfo {...review} />
-            <ReviewText {...review} />
-          </div>
-        ))}
-      </div>
+      <h1 className="mb-5 text-center text-2xl font-medium md:mb-7">
+        &apos;{decodedQuery}&apos;
+      </h1>
+      {!searchResults.length ? (
+        <div className="py-32 text-center text-lg">
+          <p>아직 등록된 리뷰가 없습니다.</p>
+          <p className="text-gray600">이 영화의 첫 번째 리뷰를 남겨보세요.</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 justify-between gap-12 lg:grid-cols-[380px_380px] lg:gap-14">
+          {searchResults.map((review: Review) => (
+            <div key={review.id}>
+              <ReviewHeader {...review} />
+              <ReviewInfo {...review} />
+              <ReviewText {...review} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
