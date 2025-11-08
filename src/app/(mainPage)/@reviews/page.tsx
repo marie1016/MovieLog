87129cb82241dcdef07c19f1da414d81065b0545
marie1016 <kjo5281@gmail.com>
2025-11-08 18:10:54 +0900

    fix: 리뷰피드 width 수정

--- src/app/(mainPage)/@reviews/page.tsx
+++ src/app/(mainPage)/@reviews/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export default function ReviewsPage() {
   return (
     <div className="flex w-full flex-col-reverse items-start gap-9 lg:flex-row lg:gap-12">
-      <section className="lg:w-3/4">
+      <section className="w-full lg:w-3/4">
         <h1 className="mb-5 w-full text-2xl font-medium md:mb-7">피드</h1>
         <ReviewFeed />
       </section>
