3652840246906c9d435b9ba83b520cc0e72f986d
marie1016 <kjo5281@gmail.com>
2025-11-13 20:14:31 +0900

    fix: 이미지 fill 경고 해결

--- src/components/ui/review/PosterImage.tsx
+++ src/components/ui/review/PosterImage.tsx
@@ -17,7 +17,7 @@ export default function PosterImage({
   const fullPosterPath = `${BASE_POSTER_PATH}/w500${posterPath}`;
   const dimensions =
     size === "lg"
-      ? ["sm:h-[256px] sm:w-[216px]", "sm:w-[150px]"]
+      ? ["sm:h-[256px] sm:w-[216px]", "sm:w-[150px] w-[90px]"]
       : ["h-[155px] w-[130px]", "w-[90px]"];
 
   return (
@@ -27,24 +27,19 @@ export default function PosterImage({
         "flex h-[155px] w-[130px] shrink-0 items-center justify-center rounded-xl border border-gray shadow-lg",
       )}
     >
-      <div
-        className={clsx(
-          "relative aspect-[2/3] w-[90px] overflow-hidden",
-          dimensions[1],
-        )}
+      <Link
+        href={`/searchReviews?query=${title}`}
+        className={clsx("relative aspect-[2/3] overflow-hidden", dimensions[1])}
       >
-        {" "}
-        <Link href={`/searchReviews?query=${title}`}>
-          <Image
-            src={fullPosterPath}
-            alt="영화 포스터"
-            sizes="90px"
-            fill
-            priority
-            className="transition-transform duration-300 ease-in-out hover:scale-110"
-          />
-        </Link>
-      </div>
+        <Image
+          src={fullPosterPath}
+          alt="영화 포스터"
+          sizes="90px"
+          fill
+          style={{ objectFit: "cover" }}
+          className="transition-transform duration-300 ease-in-out hover:scale-110"
+        />
+      </Link>
     </div>
   );
 }
